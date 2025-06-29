ba13bbb92372d257efb8fb8d5049dc7f58bebbc7
Omkar <omkargharat578@gmail.com>
2025-06-29 19:05:35 +0530

    Refactor .eleventy.js to improve directory structure and add passthrough copy for Main Components

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,18 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.setTemplateFormats(["html"]);
     eleventyConfig.addPassthroughCopy("css");
+    // eleventyConfig.addPassthroughCopy("Common");
+    // eleventyConfig.addPassthroughCopy("main-components/*.html");
+    eleventyConfig.addPassthroughCopy("Main Components/**/*");
+
     return {
         dir: {
             input: ".",
-            includes: "_includes",
+            includes: "_includes", "main-components": "Main Components",
             output: "_site",
             templateFormats: ["njk", "html", "md", "liquid", /* others you need */]
-        }
+        },
+        htmlTemplateEngine: "njk"
+
     };
 };
